c0a1f8a27f34707a5df07f55f0d19e87d0302ca9
RafaelPepe15 <rafaelpepe@usp.br>
2026-03-23 19:49:05 -0300

    Adiciona script analise.R com cálculo de média

--- analise.R
+++ analise.R
@@ -1,20 +1,7 @@
-'''[r]
-x <- c(1, 2, 3, 4, 5)
-
-media <- mean(x)
-
-print(media)
-'''
-
 # Exemplo básico
 x <- c(1, 2, 3, 4, 5)
 
 media <- mean(x)
+
 print(media)
 
-# NOVO CÓDIGO 
-desvio_padrao <- sd(x)
-soma <- sum(x)
-
-print(desvio_padrao)
-print(soma)
